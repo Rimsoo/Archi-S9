c444fa13334384b3e8e630d8463142e4fce0d729
rimsoo <abdeldebeche@gmail.com>
2021-09-21 11:28:40 +0200

    Addend controllers

--- api/controller/passager.controller.js
+++ api/controller/passager.controller.js
@@ -1,10 +1,23 @@
+import { Passager } from "../model/passager.js"
+
 export class PassagerController
 {
-	passagers = [
+	static passagers = [
 		new Passager("toto", "tata"),
 		new Passager("test", "toast"),
 		new Passager("Pass", "un"),
 		new Passager("Poiass", "deux"),
 	]
 	
+	getAll()
+	{
+		return PassagerController.passagers
+	}
+
+	getPassager(id)
+	{
+		return PassagerController.passagers[id]
+	}
+
+	getReservation()
 }
