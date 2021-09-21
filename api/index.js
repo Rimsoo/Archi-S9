2747f92b75e16bd6c7dcb522bbf54b6a8e46cd7b
rimsoo <abdeldebeche@gmail.com>
2021-09-21 16:27:14 +0200

    Add allowed CORS !!

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 import { buildRoute } from "./route/index.js"
 import express from 'express'
+import cors from 'cors'
+
 
 const app = express()
 app.use(express.json())
+
+const allowedOrigins = ['http://localhost:4200']
+
+const options = {
+  origin: allowedOrigins
+}
+app.use(cors(options))
+
 
 buildRoute(app)
 
